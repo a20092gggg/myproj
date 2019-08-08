8e512cbfcd15431c00a71303e178a6fc43952252
a20092gggg <53824473+a20092gggg@users.noreply.github.com>
2019-08-08 22:04:57 +0800

    Add files via upload

---
+++ newjack.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[3]:
+
+
+a = np.random.randint(10)
+a
+
+
+# In[6]:
+
+
+b = input(">> \n")
+
+
+# In[7]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
